84a01a3f1692b14e0dfb9d90bfc348282878342f
Unknown <aspruce@mail.csuchico.edu>
2017-04-25 11:49:46 -0700

    Part 1

--- gallery.js
+++ gallery.js
@@ -1,6 +1,18 @@
-
+var chico = [ "Woodstock", "Chico Creek Nature Center", "Oak Way Park", "Chico State Park", "Bidwell Mansion Historic Park"];
+var current = -1;
 
 function shuffleGallery()
 {
-
+  var rand = Math.random();
+  rand = rand * 4;
+  if (parseInt(rand) == current)
+  {
+    while(parseInt(rand) == current)
+    {
+      var rand = Math.random();
+      rand = rand * 4;
+    }
+  }
+  console.log(chico[parseInt(rand)]);
+  current = parseInt(rand);
 }
